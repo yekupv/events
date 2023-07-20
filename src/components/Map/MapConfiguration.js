4cc6ad2457cecbe9d33e387ec4df56aef7360315
Makhambet <krutchdd@gmail.com>
2023-07-20 07:51:59 +0600

    adding env vars

--- src/components/Map/MapConfiguration.js
+++ src/components/Map/MapConfiguration.js
@@ -1,5 +1,4 @@
-const GOOGLE_MAPS_API_KEY = import.meta.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-const test = import.meta.env.test;
+const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY;
 export const mapOptions = {
-	googleMapApiKey: "AIzaSyCuRZ03afP5HoZ6FoXFCamjkMd_5hTeKWM",
+	googleMapApiKey: GOOGLE_MAPS_API_KEY,
 };
